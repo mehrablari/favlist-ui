6266db5e27e801e74d4d703d6f5c861ffd22b664
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-07-06 16:38:59 +0100

    searchbox

--- src/utils/Searchbox.jsx
+++ src/utils/Searchbox.jsx
@@ -1,60 +1,116 @@
-
-
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
-import data from "./data";
+// import TextField from "@mui/material/TextField";
+// import Autocomplete from "@mui/material/Autocomplete";
+// import data from "./data";
 import SearchIcon from "@mui/icons-material/Search";
+// import { useState } from "react";
+// import Select from 'react-select';
+
+
+// export default function Highlights({answerData, activeAnswerJson}) {
+//   const [selectedOption, setSelectedOption] = useState(null);
+
+
+
+
+//   const datalist = activeAnswerJson && activeAnswerJson.map((item, id) => (
+    
+//       <div key={id}>
+//         <li>
+//           {item}
+//         </li>
+        
+//       </div>
+//   ));
+
 import { useState } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 
-import AnswerSubmit from "./AnswerSubmit";
+export default function Highlights({ answerData, activeAnswerJson }) {
+  const [selectedOption, setSelectedOption] = useState(null);
 
-export default function Highlights(answerData) {
-  const [selectedOption, setSelectedOption] = useState([]);
+  const datalist = activeAnswerJson && activeAnswerJson.map((item, id) => (
+    {  id: `${item}-${id}`,label: item, value: item }
+  ));
 
-  
-  
-console.log(answerData)
-  const handleOptionSelect = (event, value) => {
-    setSelectedOption(value);
-  };
-
-  const isButtonActive = selectedOption && selectedOption.length >= 3;
-  const isAutocompleteDisabled = selectedOption && selectedOption.length >= 5;
+  const isOptionEqualToValue = (option, value) => option.value === value.value;
 
   return (
-    <div className="flex flex-col justify-center mx-auto bg-neutral w-full h-full pt-[80px]">
-      <Autocomplete
-        className="bg-neutral mx-auto w-[327px]"
-        id="highlights-demo"
-        options={data}
-        getOptionLabel={(option) => option.title}
-        onChange={handleOptionSelect}
-        disabled={isAutocompleteDisabled}
-        renderInput={(params) => (
-          <TextField
-            fullWidth
-            color="secondary"
-            id="fullWidth"
-            {...params}
-            InputProps={{
-              ...params.InputProps,
-              placeholder: "Start typing an answer",
-              startAdornment: (
-                <>
-                  <SearchIcon color="action" />
-                  {params.InputProps.startAdornment}
-                </>
-              ),
-              endAdornment: null,
-            }}
-          />
-        )}
-      />
-      <AnswerSubmit selectedOption={selectedOption} isButtonActive={isButtonActive} />
+    <div className="flex flex-col justify-center mx-auto bg-neutral w-full h-full p-[60px] ">
+      {
+        datalist ? (
+        <Autocomplete
+        className="w-[327px] mx-auto h-[44px] rounded-[20px]"
+          value={selectedOption}
+          onChange={(event, newValue) => {
+            setSelectedOption(newValue);
+          }}
+          options={datalist}
+          getOptionLabel={(option) => option.label}
+          isOptionEqualToValue={isOptionEqualToValue}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+
+              InputProps={{
+                ...params.InputProps,
+                placeholder: "Start typing an answer",
+                startAdornment: (
+                  <>
+                    <SearchIcon color="action" />
+                    {params.InputProps.startAdornment}
+                  </>
+                ),
+                endAdornment: null,
+              }}
+            />
+          )}
+        />) : null
+      }
     </div>
   );
 }
 
+
+
+
+
+ // if(!answerData) {
+//   return <p>Loading</p>
+// }
+  // console.log("this is answerdata:",answerData)
+
+  // const isButtonActives = selectedOption && selectedOption.length >= 3;
+  // const isAutocompleteDisabled = selectedOption && selectedOption.length >= 5;
+
+
+// {/* <Autocomplete
+// className="bg-neutral mx-auto w-[327px]"
+// id="highlights-demo"
+// options={datalist}
+// getOptionLabel={(option) => option}
+// // onChange={handleOptionSelect}
+// // disabled={isAutocompleteDisabled}
+// renderInput={(params) => (
+//   <TextField
+//     fullWidth
+//     color="secondary"
+//     id="fullWidth"
+//     {...params}
+    // InputProps={{
+    //   ...params.InputProps,
+    //   placeholder: "Start typing an answer",
+    //   startAdornment: (
+    //     <>
+    //       <SearchIcon color="action" />
+    //       {params.InputProps.startAdornment}
+    //     </>
+    //   ),
+    //   endAdornment: null,
+    // }}
+//   />
+// )}
+// /> */}
 
 // import TextField from "@mui/material/TextField";
 // import Autocomplete from "@mui/material/Autocomplete";
