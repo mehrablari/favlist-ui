956eee49a3c481511ebf3afef35463f652102e45
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-07-21 03:51:11 +0100

    hamburger issue and swiping to fresh question

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,12 +7,8 @@ import soundEffect from "../assets/audio/software.wav";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-
-
-
-
 const Layout = () => {
-//state management
+  //state management
   const [apiData, setApiData] = useState([]);
   const [activeAnswerJson, setActiveAnswerJson] = useState(null);
   const [selectedOption, setSelectedOption] = useState([]);
@@ -20,35 +16,42 @@ const Layout = () => {
   const [answers, setAnswers] = useState([]);
   const [clickedValue, setClickedValue] = useState([]);
   const [filteredOptions, setFilteredOptions] = useState();
-  const [questionId, setQuestionId] = useState("")
+  const [questionId, setQuestionId] = useState("");
+  const [questionName, setQuestionName] = useState("");
+  const [swiperClear, setSwiperClear] = useState([]);
 
-//sound when a suggestion is clicked
+  //sound when a suggestion is clicked
   const audio = new Audio(soundEffect);
   const playSoundEffect = () => {
     audio.play();
   };
 
-
   //manage when a suggestion is clicked
   const handleClick = (option) => {
-    if(!answers.includes(option) && answers.length < 5 ){
+    if (!answers.includes(option) && answers.length < 5) {
       setAnswers((prevItems) => [...prevItems, option]);
       playSoundEffect();
-    } 
-
+    }
   };
-//manage the swiping card of question container
+
+  // Clearing the answers array & Toggling swiperClear to trigger AnsweredList update
+  const handleSwiperClear = () => {
+    setAnswers([]);
+    setSwiperClear(!swiperClear); //
+  };
+
+  //manage the swiping card of question container
   const handleSwipe = (activeQuestion) => {
     setActiveAnswerJson(activeQuestion.answersJson);
     setSelectedOption(activeQuestion.answersJson[0]);
     setSuggestedOption(activeQuestion.answersJson);
-    setQuestionId(activeQuestion.id);    
-
+    setQuestionId(activeQuestion.id);
+    setQuestionName(activeQuestion.text);
+    setSwiperClear(activeQuestion.answersJson[""]);
   };
 
   //handle filtering when user search via searchbar
   const handleFilter = (inputValue) => {
-   
     if (inputValue) {
       const suggestedOptions = [...suggestedOption];
       const options =
@@ -68,20 +71,16 @@ const Layout = () => {
       !answers.some((answer) => answer === selectedOption) &&
       answers.length < 5
     ) {
-      setAnswers(prevAnswers => [...prevAnswers, selectedOption])
+      setAnswers((prevAnswers) => [...prevAnswers, selectedOption]);
     }
   };
 
   //manage removal of answers from answeredlist
   const handleDismiss = (index) => {
-    setAnswers(prevAnswers => prevAnswers.filter((_, i) => i !== index))
+    setAnswers((prevAnswers) => prevAnswers.filter((_, i) => i !== index));
   };
 
-
-
-
-
-// manage api request
+  // manage api request
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -96,14 +95,14 @@ const Layout = () => {
           }
         );
         setApiData(response.data);
-        console.log("response", response.data)
 
         setActiveAnswerJson(response.data[0].answersJson);
         setSelectedOption(response.data[0].answersJson[0]);
         setSuggestedOption(response.data[0].answersJson);
         setClickedValue(response.data[0].answersJson);
         setQuestionId(response.data[0].id);
-        
+        setQuestionName(response.data[0].text);
+        setSwiperClear(response.data[0].answersJson[""]);
       } catch (error) {
         console.error("Error fetching API data:", error);
       }
@@ -112,14 +111,13 @@ const Layout = () => {
     fetchData();
   }, []);
 
-
   // //save current answer to localstorage
   // useEffect(() => {
   //   // Save answers to localStorage
   //   localStorage.setItem("answers", JSON.stringify(answers));
   // }, [answers]);
 
-// Retrieve answers from localStorage
+  //Retrieve answers from localStorage
   useEffect(() => {
     const storedAnswers = localStorage.getItem("answers");
     if (storedAnswers) {
@@ -130,14 +128,18 @@ const Layout = () => {
   return (
     <div className="m-w-[375px]">
       <NavBar />
-      <CardSwipeContainer questionData={apiData} handleSwipe={handleSwipe} />
+      <CardSwipeContainer
+        questionData={apiData}
+        handleSwipe={handleSwipe}
+        onSwipe={handleSwiperClear}
+      />
       <Searchbox
         answerData={apiData}
         activeAnswerJson={activeAnswerJson}
         handleSubmission={handleSubmission}
         handleFilter={handleFilter}
       />
-    <Suggestion
+      <Suggestion
         className="bg-neutral"
         suggestedOption={suggestedOption}
         setSuggestedOption={setSuggestedOption}
@@ -149,9 +151,9 @@ const Layout = () => {
         answers={answers}
         handleDismiss={handleDismiss}
         questionId={questionId}
+        questionName={questionName}
         clickedValue={clickedValue}
       />
-      
     </div>
   );
 };
--- src/components/questionbox/AnsweredList.jsx
+++ src/components/questionbox/AnsweredList.jsx
@@ -2,14 +2,14 @@ import AnswerHeader from "./AnswerHeader";
 import AnswerSettings from "./AnswerSettings";
 import DismissIcon from "../../assets/icons/Dismiss.svg";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
-const AnsweredList = ({ answers, handleDismiss, questionId}) => {
+const AnsweredList = ({ answers, handleDismiss, questionId, questionName}) => {
 
-  const [showIndex, setShowIndex] = useState(false) 
+  const [showIndex, setShowIndex] = useState(false);
   const handlePreviewAnswers = () => {
-    console.log("answers",answers)
+    
     localStorage.setItem("answers", JSON.stringify(answers));
     // Handle the preview answers logic
   };
@@ -19,21 +19,23 @@ const AnsweredList = ({ answers, handleDismiss, questionId}) => {
     event.preventDefault();
   };
 
-  const handleChange = (value) => {
-    setShowIndex(value)
-}
-
+ 
+  const handleChange = (isChecked) => {
+    setShowIndex(isChecked);
+  };
 
   return (
-    <form className="bg-neutral pt-[12px] pb-[30px] gap-[12px]" onClick={handleSubmit}>
-      <AnswerHeader handleChange={handleChange}/>
+    <form
+      className="bg-neutral pt-[12px] pb-[30px] gap-[12px]"
+      onClick={handleSubmit}
+    >
+      <AnswerHeader handleChange={handleChange} />
 
       {answers.length === 0 && <AnswerSettings />}
 
       {answers.length > 0 &&
         answers.map((answer, index) => (
           <div className="bg-neutral w-[327px] mx-auto" key={index}>
-            
             <div className="bg-neutral p-[8px] flex flex-row justify-between mx-auto w-[327px]">
               <div className="w-[327px] bg-button-inactive rounded-lg font-[400] text-gray-light px-[12px] py-[10px] h-[44px] mx-auto flex flex-row justify-between">
                 <span className="text-[13px] h-[16px]">{answer}</span>
@@ -44,23 +46,28 @@ const AnsweredList = ({ answers, handleDismiss, questionId}) => {
                 />
               </div>
             </div>
-            {setShowIndex === true && 
-            <div className="bg-neutral p-[8px] flex flex-row justify-between mx-auto w-[327px]">
-              <span className="">{index+1}</span>
-              <div className="w-[327px] bg-button-inactive rounded-lg font-[400] text-gray-light px-[12px] py-[10px] h-[44px] mx-auto flex flex-row justify-between">
-                <span className="text-[13px] h-[16px]">{answer}</span>
-                <img
-                  src={DismissIcon}
-                  alt="dismiss icon"
-                  onClick={() => handleDismiss(index)}
-                />
+            {/* {setShowIndex && (
+              <div className="bg-neutral p-[8px] flex flex-row justify-between mx-auto w-[327px]">
+                <span className="">{index + 1}</span>
+                <div className="w-[327px] bg-button-inactive rounded-lg font-[400] text-gray-light px-[12px] py-[10px] h-[44px] mx-auto flex flex-row justify-between">
+                  <span className="text-[13px] h-[16px]">{answer}</span>
+                  <img
+                    src={DismissIcon}
+                    alt="dismiss icon"
+                    onClick={() => handleDismiss(index)}
+                  />
+                </div>
               </div>
-            </div>}
+            )} */}
           </div>
         ))}
 
       {answers.length > 0 && (
-        <Link to="/preview" state={{answers, questionId}} className="flex justify-center align-middle pt-[10px]">
+        <Link
+          to="/preview"
+          state={{ answers, questionId, questionName }}
+          className="flex justify-center align-middle pt-[10px]"
+        >
           <button
             className={`h-[40px] text-center rounded-lg font-[600] text-[14px] w-[310px] mx-auto ${
               answers.length >= 3
@@ -68,11 +75,10 @@ const AnsweredList = ({ answers, handleDismiss, questionId}) => {
                 : "bg-gray-purple cursor-not-allowed text-neutral"
             }`}
             type="submit"
-            disabled={answers.length < 3 }
+            disabled={answers.length < 3}
             onClick={handlePreviewAnswers}
           >
             Preview Answers
-            
           </button>
         </Link>
       )}
@@ -81,4 +87,3 @@ const AnsweredList = ({ answers, handleDismiss, questionId}) => {
 };
 
 export default AnsweredList;
-
