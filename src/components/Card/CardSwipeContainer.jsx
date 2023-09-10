6d82251510545c9d4b5e37a09d0039b0874eabec
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-09-10 01:43:33 +0100

    youtube logo fixed

--- src/components/Card/CardSwipeContainer.jsx
+++ src/components/Card/CardSwipeContainer.jsx
@@ -7,10 +7,12 @@ import youtubeIcon from "../../assets/images/youtubeIcon.jpg";
 import Clock from "../../assets/images/clock.png";
 import { useState } from "react";
 
-
-const CardSwipeContainer = ({ apiData, handleSwipe,questionId }) => {
- 
-
+const CardSwipeContainer = ({
+  apiData,
+  handleSwipe,
+  questionId,
+  daysRemaining,
+}) => {
   const storedQuestionIndex =
     localStorage.getItem("selectedQuestionIndex") || 0;
   const [question, setQuestion] = useState(storedQuestionIndex);
@@ -18,22 +20,30 @@ const CardSwipeContainer = ({ apiData, handleSwipe,questionId }) => {
   const handleSwipeChange = (swiper) => {
     const activeIndex = swiper.activeIndex;
     setQuestion(activeIndex);
-    console.log("index", activeIndex);
 
-    
     // localStorage.setItem("questionIndex", activeIndex);
     // handleSwipe(questionData[activeIndex]);
     handleSwipe(apiData[activeIndex], activeIndex);
-    
   };
+  const generateRandomColors = (count) => {
+    const colors = [];
+    for (let i = 0; i < count; i++) {
+      const randomColor = `#${Math.floor(Math.random() * 16777215).toString(
+        16
+      )}`;
+      colors.push(randomColor);
+    }
+    return colors;
+  };
+  const borderColors = generateRandomColors(apiData.length);
 
   const remaining = (days) => {
     if (days === 0) {
-      return "day to expiry";
+      return "day to expire";
     } else if (days === 1) {
-      return "1 day to expiry";
-    } else if (days >= 2 && days <= 7) {
-      return `${days} days to expiry`;
+      return "1 day to expire";
+    } else if (days >= 2 && days <= 20) {
+      return `${days} days to expire`;
     } else {
       return null;
     }
@@ -56,26 +66,26 @@ const CardSwipeContainer = ({ apiData, handleSwipe,questionId }) => {
   return (
     <div className="bg-primary w-full pb-[30px]">
       <Swiper
-        initialSlide={question}
+        initialSlide={questionId}
         effect={"cards"}
         grabCursor={true}
         loop={true}
         modules={[EffectCards]}
-        className="mySwiper h-[280px] sm:px-[10px] sm:py-[20px] md:px-[34px] md:py-[30px] flex justify-center sm:w-[220px] md:w-[300px] w-[327px] align-middle pt-[40px] pb-[30px] mx-auto font-sans"
+        // slidesPerView={1}
+        className="mySwiper h-[280px] align-middle mx-auto font-sans pb-[30px] pt-[40px] sm:px-[50px] sm:max-w-auto sm:py-[25px] md:px-[20px] md:py-[30px] flex flex-wrap flex-auto justify-center s:w-[270px] md:max-w-[320px] md:w-[270px] w-[327px] max-w-[327px]"
         onSlideChange={(swiper) => handleSwipeChange(swiper)}
       >
         {apiData.map((question, id) => (
           <SwiperSlide
             key={id}
-            className={`sm:w-[320px]   lg:w-[340px] bg-neutral rounded-[24px] mx-auto flex flex-col justify-center text-center gap-[10px] p-[20px] my-[3rem] sm:px-[40px] max-w-[327px] h-[270px] drop-shadow-lg border-2 border-primary overflow-hidden`}
-              // style={{ border: `2px solid ${borderColorForId(id)}` }}
-              
+            className={`sm:w-[260px] swiper-1 md:w-[310px] lg:w-[320px] bg-neutral rounded-[24px] mx-auto flex flex-col justify-center text-center gap-[10px] p-[10px] md:px-[10px] my-[3rem] sm:px-[10px] max-w-[327px] h-[270px] drop-shadow-lg border-2 overflow-hidden`}
+            style={{ borderColor: borderColors[id] }}
+            // style={{ border: `2px solid ${borderColorForId(id)}` }}
           >
-           
-            <p className="text-gray-dark text-lg items-center font-baloo2 sm:text-[16px] md:text-[16px] px-[20px] py-[5px] max-w-[340px] sm:w-[230px] md:leading-2 sm:leading-4 leading-2 font-[700] sm:font-[600] tracking-tighter">
+            <p className="text-gray-dark text-lg items-center font-baloo2 sm:text-[16px] md:text-[17px] md:px-[20px] px-[20px] py-[5px] max-w-[340px] sm:w-[250px] md:w-[260px] md:leading-2 sm:leading-4 leading-2 font-[700] sm:font-[600] tracking-tighter">
               {question.text}
             </p>
-            <div className="flex flex-col bg-neutral  rounded-lg gap-[8px] w-[320px]">
+            <div className="flex flex-col bg-neutral rounded-lg w-[320px] pt-[10px]">
               <p className="text-[13px] text-gray-light font-baloo2 font-[400] ">
                 {formatDate(question.dateToPost)}
               </p>
@@ -87,13 +97,13 @@ const CardSwipeContainer = ({ apiData, handleSwipe,questionId }) => {
                 </h1>
               </div>
 
-              <div className="flex flex-col justify-center w-[280px] h-[56px] mx-auto font-baloo2 pt-[10px]">
+              <div className="flex flex-col justify-center w-[280px] h-[56px] mx-auto font-baloo2 pt-[20px]">
                 <h3 className="text-gray-light text-[13px] font-[400]">
                   affiliate
                 </h3>
                 <div className="flex justify-center ">
                   <a href={`${question.sponsor.url}`} className="rounded-full">
-                     <img
+                    <img
                       src={question.sponsor.logoS3Url}
                       alt="netflix"
                       className="rounded-md h-[30px] w-[40px] "
@@ -101,17 +111,16 @@ const CardSwipeContainer = ({ apiData, handleSwipe,questionId }) => {
                   </a>
                 </div>
                 <div className="flex justify-center">
-                  <h3 className="text-gray-dark text-[13px] font-[600] pb-[10px] text-center w-[200px]">
+                  <h3 className="text-gray-dark text-[13px] font-[600] pb-[15px] text-center w-[200px]">
                     {question.sponsor.name}
                   </h3>
                 </div>
 
-                <div className="absolute bottom-md right-md font-baloo2 border-2 bg-primary" style={{backgroundColor:"#A13E97"}}>
-                  <a
-                    href={`${question.sponsor.adsS3Url
-                    }`}
-                    className="w-[56px]"
-                  >
+                <div
+                  className="youtube absolute bottom-md right-[0px] top-[235px] font-baloo2"
+                  // style={{ backgroundColor: "#A13E97" }}
+                >
+                  <a href={`${question.sponsor.adsS3Url}`} className="w-[56px]">
                     <img
                       src={youtubeIcon}
                       alt="youtube icon"
