6e0396fd1dc1e86bad1a38670e464a597a3914f0
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-10-05 15:05:26 +0100

    helmet added for seo

--- src/components/AnswerGraphic/AnswerGraphic.jsx
+++ src/components/AnswerGraphic/AnswerGraphic.jsx
@@ -2,7 +2,8 @@ import { Link, useLocation } from "react-router-dom";
 import { RWebShare } from "react-web-share";
 import Video from "../../assets/icons/video.svg";
 import ArrowBack from "../../assets/icons/arrowback.svg";
-import congrats from "../../assets/gif/congrats.gif"
+import congrats from "../../assets/gif/congrats.gif";
+import { Helmet } from "react-helmet-async";
 
 const AnswerGraphics = () => {
   const location = useLocation();
@@ -11,55 +12,79 @@ const AnswerGraphics = () => {
   // console.log("img link", imgUrl);
 
   return (
-    <div className="pt-[100px] sm:pt-[130px] md:pt-[130px]  pb-[10px] sm:h-screen h-screen md:full mdx:h-screen mx-auto" style={{backgroundImage:`url(${congrats})`,height:"100vh", backgroundPosition:"center",backgroundRepeat:"no-repeat", width:"100%", backgroundSize:"cover"}}>
-      <div className="bg-neutral min-h-[300px] w-[400px]  sm:w-[340px] md:w-[360px] mx-auto rounded-[24px]">
-        <div className="flex flex-row justify-center align-middle mx-auto">
-          <img src={Video} alt="video" className="h-full pt-[8px] pr-[10px]" />
-          <h1 className="font-[600] text-md text-text-blue">
-            Your Answer graphics
-          </h1>
-        </div>
-        <div className="min-h-[100px] max-w-[400px] p-[10px]">
-          <img src={imgUrl} alt="Your Answer Graphic" className=" w-[400px] sm:w-[340px] md:w-[360px] h-[340px] sm:h-[300px]" />
-        </div>
-        <div className="flex flex-col pb-[10px]">
-          <div className="flex flex-col justify-center align-middle pb-[5px]">
-            <RWebShare
-              data={{
-                text: "My answers",
-                url: imgUrl,
-                title: "Favlist",
-              }}
-              onClick={() => console.log("shared successfully!")}
-            >
-              <button className="h-[40px] w-[250px] hover:bg-opacity-75 text-center mx-auto rounded-lg  font-[600] flex-grow flex-shrink text-[14px] text-neutral bg-primary ">
-                Share to social media
-              </button>
-            </RWebShare>
-          </div>
-          <div className="flex flex-row items-center justify-center mx-auto rounded-lg h-[35px] p-[10px] bg-button-inactive min-w-[250px]">
-          <img src={ArrowBack} alt="" className="h-full pr-[2px]" />
-          <Link to="/" className="text-[14px] font-semibold text-primary-light">
-            Go back to question
-          </Link>
-        </div>
+    <>
+      <Helmet defer={false} prioritizeSeoTags>
+        <title>Give answers to your favourite things</title>
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="question image" />
 
-          {/* <Link
-            to="/"
-            className="flex flex-row items-center justify-between mx-auto rounded-lg h-[40px] p-[10px] max-w-[193px]"
-          >
+        <meta name="description" content="helmet" />
+        <meta property="og:url" content="https://favlist.net/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image" content={imgUrl} />
+        <meta
+          property="og:description"
+          content="Give answers to your favourite things"
+        />
+        <meta name="twitter:image:alt" content="favourite answers" />
+      </Helmet>
+      <div
+        className="pt-[100px] sm:pt-[130px] md:pt-[130px]  pb-[10px] sm:h-screen h-screen md:full mdx:h-screen mx-auto"
+        style={{
+          backgroundImage: `url(${congrats})`,
+          height: "100vh",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="bg-neutral min-h-[300px] w-[400px]  sm:w-[340px] md:w-[360px] mx-auto rounded-[24px]">
+          <div className="flex flex-row justify-center align-middle mx-auto">
             <img
-              src={ArrowBack}
-              alt=""
-              className="h-[18px] w-[18px] pr-[5px]"
+              src={Video}
+              alt="video"
+              className="h-full pt-[8px] pr-[10px]"
             />
-            <div className="text-[12px] font-semibold text-primary h-[50px] pt-[10px]">
-              Back to question
+            <h1 className="font-[600] text-md text-text-blue">
+              Your Answer graphics
+            </h1>
+          </div>
+          <div className="min-h-[100px] max-w-[400px] p-[10px]">
+            <img
+              src={imgUrl}
+              alt="Your Answer Graphic"
+              className=" w-[400px] sm:w-[340px] md:w-[360px] h-[340px] sm:h-[300px]"
+            />
+          </div>
+          <div className="flex flex-col pb-[10px]">
+            <div className="flex flex-col justify-center align-middle pb-[5px]">
+              <RWebShare
+                data={{
+                  text: "My answers",
+                  url: imgUrl,
+                  title: "Favlist",
+                }}
+                onClick={() => console.log("shared successfully!")}
+              >
+                <button className="h-[40px] w-[250px] hover:bg-opacity-75 text-center mx-auto rounded-lg  font-[600] flex-grow flex-shrink text-[14px] text-neutral bg-primary ">
+                  Share to social media
+                </button>
+              </RWebShare>
             </div>
-          </Link> */}
+            <div className="flex flex-row items-center justify-center mx-auto rounded-lg h-[35px] p-[10px] bg-button-inactive min-w-[250px]">
+              <img src={ArrowBack} alt="" className="h-full pr-[2px]" />
+              <Link
+                to="/"
+                className="text-[14px] font-semibold text-primary-light"
+              >
+                Go back to question
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
