d3798de2844795cb790573d40032021b07cd6432
segzy2baba <Segzy2baba1@gmail.com>
2023-12-02 00:46:00 +0100

    fix error refresh fuction in noDatacompnent

--- src/components/Error/NoDataComponent.jsx
+++ src/components/Error/NoDataComponent.jsx
@@ -1,17 +1,23 @@
 
 import PropTypes from 'prop-types'
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+// import { Link } from "react-router-dom";
 
 function NoDataComponent({message, buttonText}) {
+  const navigate = useNavigate();
+  const refreshPage = () => {
+    navigate(0);
+  };
+
   return (
     <div className="flex justify-center items-center flex-col mx-auto pt-[100px] bg-neutral h-screen">
     <div className="text-center">
       <h1 className="text-[30px]">{message}</h1>
-      <Link to="/" className="text-center">
+      <div  className="text-center"  onClick={refreshPage}>
         <button className="bg-primary text-neutral rounded-[24px] py-[15px] w-[100px] px-[15px] pb-[10px]" >
           {buttonText}
         </button>
-      </Link>
+      </div>
     </div>
   </div>
   )
