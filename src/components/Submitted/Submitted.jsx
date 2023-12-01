980a72bd3361a02b4af3ca01822a15290670866c
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-12-01 23:55:29 +0100

    social button corrected

--- src/components/Submitted/Submitted.jsx
+++ src/components/Submitted/Submitted.jsx
@@ -1,47 +1,49 @@
 import Video from "../../assets/icons/video.svg";
 import Home from "../../assets/icons/Home.svg";
-import {  useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import { useContext,  useEffect, useState} from "react";
-import "./submited.css"
+import { useContext, useEffect, useState } from "react";
+import "./submited.css";
 
 import { RWebShare } from "react-web-share";
 // import useQuestions from "../../hooks/useQuestions";
 import DataContext from "../../context/DataContexts";
 
 const Submit = () => {
-
-  const { setEditQuestion } =
-    useContext(DataContext);
+  const { setEditQuestion } = useContext(DataContext);
 
   const location = useLocation();
   const graphicUrl = location.state?.graphicUrl;
   const graphicFile = location.state?.graphicFile;
 
-
   const navigate = useNavigate();
 
   const [isMobile, setIsMobile] = useState(
-    window.matchMedia("(max-width: 915px)").matches || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    window.matchMedia("(max-width: 915px)").matches ||
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
   );
 
   useEffect(() => {
-   
-    setIsMobile(window.matchMedia("(max-width: 915px)").matches || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-
-}, []); 
+    setIsMobile(
+      window.matchMedia("(max-width: 915px)").matches ||
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+    );
+  }, []);
   // const { mutate: mutateQuestion} = useQuestions()
- 
+
   const shareToInstagram = async () => {
     try {
       // Fetch the image from the remote URL and convert it to a blob
-
 
       if (graphicFile) {
         // const blobImageAsset = await response.blob();
         const blobImageAsset = new Blob([graphicFile], { type: "image/png" });
         // setFilesArrayContent(blobImageAsset)
-    
+
         // Create a File object from the blob
         const filesArray = [
           new File([blobImageAsset], `Favlist_${new Date().getTime()}.png`, {
@@ -50,7 +52,6 @@ const Submit = () => {
           }),
         ];
 
-     
         // setFilesArrayContent(filesArray)
 
         // Set up the share data
@@ -61,7 +62,6 @@ const Submit = () => {
 
         // console.log()
 
-       
         // Check if the navigator supports sharing and the provided data
         console.log("fist", navigator.canShare);
         //console.log("second", navigator.canShare(shareData));
@@ -82,18 +82,14 @@ const Submit = () => {
     }
   };
 
-
-  const handleSubmit =  () => {
-
+  const handleSubmit = () => {
     // mutateQuestion()
-    
+
     localStorage.removeItem("answers");
     // localStorage.removeItem("selectedQuestionIndex");
     // localStorage.removeItem("selectedQuestionId");
     setEditQuestion(null);
-    navigate('/');
-  
- 
+    navigate("/");
   };
 
   return (
@@ -123,7 +119,11 @@ const Submit = () => {
         {/* twitter  */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image:alt" content="favorite answers" />
-        <meta name="twitter:image" itemProp="image" content={graphicUrl + "/"} />
+        <meta
+          name="twitter:image"
+          itemProp="image"
+          content={graphicUrl + "/"}
+        />
         <meta property="twitter:image:width" content="600" />
         <meta property="twitter:image:height" content="314" />
         <meta property="twitter:card" content="app" />
@@ -134,14 +134,11 @@ const Submit = () => {
           content="Give answers to your favorite things"
         />
       </Helmet>
-      <div
-        className="bg_submitted pt-[50px] sm:pt-[120px] md:pt-[120px] pb-[10px] h-screen md:h-screen mdx:h-screen mx-auto sm:h-screen"
-
-      >
+      <div className="bg_submitted pt-[50px] sm:pt-[120px] md:pt-[120px] pb-[10px] h-screen md:h-screen mdx:h-screen mx-auto sm:h-screen">
         <div className="bg-neutral w-[360px] sm:w-[340px] md:w-[360px] rounded-lg mx-auto py-[10px]">
           <div className="flex flex-col">
             <h2 className="w-[327px] font-[500] mx-auto text-center text-[14px] leading-5 pt-[10px]">
-              Yaayy! Your answers have been submitted! {" "}
+              Yaayy! Your answers have been submitted!{" "}
             </h2>
 
             <div className="flex flex-row justify-center">
@@ -152,41 +149,43 @@ const Submit = () => {
             </div>
           </div>
           <div>
-          <img
-            // src={graphicUrl}
-            src={URL.createObjectURL(graphicFile)}
-            alt="Your Answer Graphic"
-            className=" mx-auto p-[10px]"
-          />
+            <img
+              // src={graphicUrl}
+              src={URL.createObjectURL(graphicFile)}
+              alt="Your Answer Graphic"
+              className=" mx-auto p-[10px]"
+            />
           </div>
           <div className=""></div>
 
           <div className="flex flex-col pb-[10px] mx-auto">
-          {isMobile ? (
-            <button
-              className="h-[40px] bg-primary hover:bg-opacity-75 text-center rounded-lg font-[600] w-[250px] flex-grow flex-shrink text-[14px] text-neutral"
-              onClick={shareToInstagram}
-            >
-               Share to Social Media
-            </button>
-          ) : (
-            <div className="p-[5px] mx-auto">
-              <RWebShare
-                data={{
-                  text: "Favorite Answers",
-                  url: graphicUrl,
-                  title: "Give answers to your favorite things",
-                }}
-                onClick={() => console.log("shared successfully!")}
-              >
-                <button className="h-[40px] bg-primary hover:bg-opacity-75 text-center rounded-lg font-[600] w-[250px] flex-grow flex-shrink text-[14px] text-neutral">
+            {isMobile ? (
+              <div className="p-[5px] mx-auto">
+                <button
+                  className="h-[40px] bg-primary hover:bg-opacity-75 text-center rounded-lg font-[600] w-[250px] flex-grow flex-shrink text-[14px] text-neutral"
+                  onClick={shareToInstagram}
+                >
                   Share to Social Media
                 </button>
-              </RWebShare>
-            </div>
-          )}
+              </div>
+            ) : (
+              <div className="p-[5px] mx-auto">
+                <RWebShare
+                  data={{
+                    text: "Favorite Answers",
+                    url: graphicUrl,
+                    title: "Give answers to your favorite things",
+                  }}
+                  onClick={() => console.log("shared successfully!")}
+                >
+                  <button className="h-[40px] bg-primary hover:bg-opacity-75 text-center rounded-lg font-[600] w-[250px] flex-grow flex-shrink text-[14px] text-neutral">
+                    Share to Social Media
+                  </button>
+                </RWebShare>
+              </div>
+            )}
             <span
-             onClick={handleSubmit}
+              onClick={handleSubmit}
               className="flex flex-row bg-primary items-center justify-center mx-auto rounded-lg h-[40px] hover:bg-primary-lighter px-[10px] w-[250px]"
             >
               <img
