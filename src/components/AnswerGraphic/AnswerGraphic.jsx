709fccb9024da9016b9b6b1976b4a4f7c7c7e21b
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-12-05 16:27:43 +0100

    background img answergraphics

--- src/components/AnswerGraphic/AnswerGraphic.jsx
+++ src/components/AnswerGraphic/AnswerGraphic.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import {  useState } from "react";
+import { useState } from "react";
 // import axios from "axios";
 import { RWebShare } from "react-web-share";
 import Video from "../../assets/icons/video.svg";
@@ -9,10 +9,9 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Helmet } from "react-helmet-async";
 import { useEffect } from "react";
-import "./answergraphic.css"
+import "./answergraphic.css";
 
 const AnswerGraphics = () => {
-
   const location = useLocation();
   const dataContainer = location.state;
   const imgUrl = dataContainer?.answerGraphicLink;
@@ -20,29 +19,31 @@ const AnswerGraphics = () => {
   // console.log(imgUrl);
 
   // const [isMobile, setIsMobile] = useState(
-  //   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) 
+  //   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
   // );
   const [isMobile, setIsMobile] = useState(
-    window.matchMedia("(max-width: 915px)").matches || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    window.matchMedia("(max-width: 915px)").matches ||
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
   );
 
-
   useEffect(() => {
-   
-      setIsMobile(window.matchMedia("(max-width: 915px)").matches || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-
-  
+    setIsMobile(
+      window.matchMedia("(max-width: 915px)").matches ||
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+    );
   }, []);
 
   // useEffect(() => {
 
   //     setIsMobile(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
- 
-  // }, []); 
 
+  // }, []);
 
-  console.log('working',isMobile)
-
+  console.log("working", isMobile);
 
   const shareToInstagram = async () => {
     try {
@@ -61,10 +62,9 @@ const AnswerGraphics = () => {
         for (let i = 0; i < binaryData.length; i++) {
           arrayBuffer[i] = binaryData.charCodeAt(i);
         }
-        const blobImageAsset = new Blob([arrayBuffer ], { type: "image/png" });
-        console.log(blobImageAsset)
+        const blobImageAsset = new Blob([arrayBuffer], { type: "image/png" });
+        console.log(blobImageAsset);
         // setFilesArrayContent(blobImageAsset)
-      
 
         // Create a File object from the blob
         const filesArray = [
@@ -95,18 +95,16 @@ const AnswerGraphics = () => {
           return true; // Sharing successful
         } else {
           console.error("Web Share API is not supported on this browser.");
-          toast.error("Web Share API is not supported on this browser.")
+          toast.error("Web Share API is not supported on this browser.");
           return false; // Sharing failed
         }
       }
     } catch (error) {
       console.error("Error sharing image to Instagram:", error);
-      toast.error("Error sharing image to Instagram")
+      toast.error("Error sharing image to Instagram");
       return false; // Sharing failed
     }
   };
-
-
 
   return (
     <>
@@ -125,10 +123,7 @@ const AnswerGraphics = () => {
         />
         <meta name="twitter:image:alt" content="favourite answers" />
       </Helmet>
-      <div
-        className="graphics_img pt-[100px] sm:pt-[130px] md:pt-[130px]  pb-[10px] sm:h-screen h-screen md:full mdx:h-screen mx-auto"
-        
-      >
+      <div className="graphics_img pt-[100px] sm:pt-[130px] md:pt-[130px]  pb-[10px] sm:h-screen h-screen md:full mdx:h-screen mx-auto">
         <div className="bg-neutral min-h-[300px] w-[400px]  sm:w-[340px] md:w-[360px] mx-auto rounded-[24px]">
           <div className="flex flex-row justify-center align-middle mx-auto">
             <img
@@ -140,39 +135,48 @@ const AnswerGraphics = () => {
               Your Answer graphics
             </h1>
           </div>
-          <div className="min-h-[100px] max-w-[400px] p-[10px]">
-            <img
+          <div
+            className="p-[10px] mx-auto"
+            style={{
+              backgroundImage: `url(${imgUrl})`,
+              height: "310px",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              width: "300px",
+              backgroundSize: "cover",
+            }}
+          >
+            {/* <img
               src={imgUrl}
               alt="Your Answer Graphic"
               className=" w-[400px] sm:w-[340px] md:w-[360px] h-[340px] sm:h-[300px]"
-            />
+            /> */}
           </div>
-          <div className="flex flex-col pb-[10px]">
+          <div className="flex flex-col py-[10px]">
             <div className="flex flex-col justify-center align-middle pb-[5px]">
- 
               {isMobile ? (
-              <button
-                className="h-[40px] w-[320px] hover:bg-opacity-75 text-center mb-[7px] mx-auto rounded-lg  font-[600] flex-grow flex-shrink text-[14px] text-neutral bg-primary "
-                onClick={shareToInstagram}
-              >
-                Share to Instagram
-              </button>
-                 ) : (
-              <RWebShare
-                data={{
-                  text: "My answers",
-                  url: imgUrl,
-                  title: "Favlist",
-                }}
-                onClick={() => console.log("shared successfully!")}
-              >
-                <button className="h-[40px] w-[320px] hover:bg-opacity-75 text-center mx-auto rounded-lg  font-[600] flex-grow flex-shrink text-[14px] text-neutral bg-primary ">
-                  Share to social media
+                <button
+                  className="h-[40px] w-[320px] hover:bg-opacity-75 text-center mb-[7px] mx-auto rounded-lg  font-[600] flex-grow flex-shrink text-[14px] text-neutral bg-primary "
+                  onClick={shareToInstagram}
+                >
+                  Share to Instagram
                 </button>
-              </RWebShare>
-               )}
+              ) : (
+                <RWebShare
+                  data={{
+                    text: "My answers",
+                    url: imgUrl,
+                    title: "Favlist",
+                  }}
+                  onClick={() => console.log("shared successfully!")}
+                >
+                  <button className="h-[40px] w-[320px] hover:bg-opacity-75 text-center mx-auto rounded-lg  font-[600] flex-grow flex-shrink text-[14px] text-neutral bg-primary ">
+                    Share to social media
+                  </button>
+                </RWebShare>
+              )}
             </div>
-           
+
             <div className="flex flex-row items-center justify-center mx-auto rounded-lg h-[35px] p-[10px] bg-button-inactive min-w-[320px]">
               <img src={Home} alt="" className="h-full pr-[2px]" />
               <Link
